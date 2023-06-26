e69b4652248583590a56e025b44e3e8f4b5fbedb
cletusola <olamicolo@gmail.com>
2023-06-26 04:30:08 -0400

    modified users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+User = get_user_model() 
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages 
+from django.contrib.auth.decorators import login_required
