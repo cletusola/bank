3e9567f2f3e2f44de7404f170bd0497572ae7895
cletusola <olamicolo@gmail.com>
2023-06-26 04:23:09 -0400

    modified users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+# custom user admin display 
+class CustomeUserAdmin(UserAdmin):
+
+    model = CustomUser
+
+    list_display = ['username','email','is_active','is_staff','is_superuser','last_login']
+    list_display_links = ['username','email']
+    list_filter = ['username','email','is_active','is_staff','is_superuser']
+    fieldsets = [
+        ('Basic Info', {'fields':('email',)}),
+        ('Permissions', {'fields':('is_active','is_staff','is_superuser',
+                        'groups','user_permissions')}),
+        ('Dates', {'fields': ('last_login',)})
+    ]
+    ordering = ('email',)
+    search_fields = ['username','email']
+
+admin.site.register(CustomUser,CustomeUserAdmin)
