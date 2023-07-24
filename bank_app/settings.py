8d9488b75532e3f9399f35fa1b16e055ffd67816
cletusola <olamicolo@gmail.com>
2023-07-24 10:48:35 -0400

    modified: bank_app/settings.py

--- bank_app/settings.py
+++ bank_app/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'bank_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates'), os.path.join(BASE_DIR,'account/templates'), os.path.join(BASE_DIR,'users/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
