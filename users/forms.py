5af8c3176870108c9e7f376e456e04685886eb1e
cletusola <olamicolo@gmail.com>
2023-07-25 06:22:32 -0400

    modified users/forms.py

--- users/forms.py
+++ users/forms.py
@@ -8,8 +8,8 @@ from django.contrib.auth.models import User
 
 
 """ sign in form """
-class SignIn(forms.Form):
-    username = forms.CharField(min_length=5, max_length=20, required=True)
-    password = forms.CharField(min_length=8, max_length=30, required=True, widget=forms.PasswordInput)
+class LogInForm(forms.Form):
+    username = forms.CharField(max_length=20, required=True)
+    password = forms.CharField(max_length=30, required=True, widget=forms.PasswordInput)
 
 
