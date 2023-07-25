e4a7ac76600d151213cc02a8718292f15cf20be7
cletusola <olamicolo@gmail.com>
2023-07-25 07:41:50 -0400

    modified bank_app/urls.py

--- bank_app/urls.py
+++ bank_app/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 
+
+from account.views import home 
+
 urlpatterns = [
+    path('', home, name="home"),
     path('admin/', admin.site.urls),
     path('account/', include("account.urls")),
     path('users/', include("users.urls")),
