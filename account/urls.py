2e83ebee04e5301e05ba517c8a757917bc0fe101
cletusola <olamicolo@gmail.com>
2023-07-25 05:13:10 -0400

    modified account/urls.py

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import (
     register_account,
     user_info,
-    dashboard
+    dashboard,
+    money_transfer,
+    confirm_transaction,
 )
 
 
@@ -12,4 +14,6 @@ urlpatterns = [
     path('join/register/', register_account, name='register'),
     path('join/user_info/', user_info, name="user_info"),
     path('pages/dashboard/', dashboard, name="dashboard"),
+    path('transactions/money_transfer/', money_transfer, name='money_transfer'),
+    path('transactions/confirm_transaction/', confirm_transaction, name="confirm_transaction"),
 ]
