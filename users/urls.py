e6315e6e1b30ba1a1d84888837edb77567427183
cletusola <olamicolo@gmail.com>
2023-07-26 05:56:50 -0400

    modified users/urls.py file

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import (
     login_request,
-    logout_request
+    logout_request,
+    change_password,
 )
 
 
 urlpatterns = [
     path('join/login/', login_request, name="login"),
     path('join/logout/', logout_request, name="logout"),
+    path('pages/change_password', change_password, name="change_password"),
 ]
