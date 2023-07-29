5d5406994a20a462c805073b29a3b29db08b4b7b
cletusola <olamicolo@gmail.com>
2023-07-29 06:59:06 -0400

    modified account/admin.py

--- account/admin.py
+++ account/admin.py
@@ -6,7 +6,7 @@ from .models import Account, Account_History
 
 # account admin 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ['account_name','account_owner','account_number','account_owner','date_opened']
+    list_display = ['account_name','account_number','account_owner','date_opened']
     list_display_links = ['account_name','account_number','account_owner','date_opened']
 
 admin.site.register(Account, AccountAdmin)
